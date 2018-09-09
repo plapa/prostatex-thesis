b951891b407ff8eebff32c968d0de14767f0dc8f
PLAPA <lapadsk@gmail.com>
2018-09-09 12:41:02 +0100

    initial commit

---
+++ src/data/01_create_metadata.py
@@ -0,0 +1,13 @@
+import os
+import pandas as pd
+
+images_info_path = "../data/raw/Train_Information/ProstateX-Images-Train.csv"
+findings_info_path = "../data/raw/Train_Information/ProstateX-Findings-Train.csv
+
+images_info = pd.read_csv(images_info_path)
+
+findings_info = pd.read_csv(findings_info_path)
+
+metadata_labels = pd.merge(images_info, findings_info, left_on=["ProxID", "fid", "pos"], right_on=["ProxID", "fid", "pos"])
+
+metadata_labels.to_csv("../data/interim/train_information.csv")
