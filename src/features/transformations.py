53aef785ad728e7f9bcd6ce870bd0610caf9482d
PLAPA <lapadsk@gmail.com>
2018-11-01 23:26:18 +0000

    Implemente basic equalize trans hist augmentation

--- src/features/transformations.py
+++ src/features/transformations.py
@@ -1,6 +1,9 @@
-from src.helper import get_config
 from imgaug import augmenters as iaa
+from imgaug.augmenters import Augmenter
+from skimage import exposure
 import numpy as np
+
+from src.helper import get_config
 
 def normalize_meanstd(x, axis=(1,2)): 
     # axis param denotes axes along which mean & std reductions are to be performed
@@ -43,5 +46,19 @@ def create_augmenter():
 
     return seq
 
+class Equalize_Hist(Augmenter):
+    def __init__(self,name=None, deterministic=False, random_state=None, per_channel = False):
+        super(Equalize_Hist, self).__init__(name=name, deterministic=deterministic, random_state=random_state)
+        
+        self.per_channel = False
 
+    def _augment_images(self, images, random_state, parents, hooks):
+        for i in range(len(images)):
+                images[i] = exposure.equalize_hist(images[i])
+        return images
 
+    def _augment_keypoints(self, keypoints_on_images, random_state, parents, hooks):
+        return keypoints_on_images
+
+    def get_parameters(self):
+        return [self.p]
