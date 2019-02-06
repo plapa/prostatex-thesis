07656a57acc0440e88e6f0dfd85aac61f1e287de
PLAPA <lapadsk@gmail.com>
2019-02-06 16:18:31 +0000

    changing organization of augmentation

--- src/features/transformations.py
+++ src/features/transformations.py
@@ -47,105 +47,108 @@ def normalize_01(x, axis=None):
 
     return x
 
-def create_augmenter():
 
-    hist_use = config["preprocessing"]["augmentation"]["histogram_method"]
+if config["preprocessing"]["use_augmentation"]:
 
-    if hist_use == "rescale":
-        hist_fun = RescaleIntensity
-    elif hist_use == "CLAHE":
-        hist_fun = iaa.CLAHE
-    elif hist_use == "hist_equalization":
-        hist_fun = iaa.AllChannelsHistogramEqualization
-    else:
-        hist_fun = None
-    print("AAAA")
+    def create_augmenter():
 
-    list_augmenters = [
-        iaa.SomeOf((0,5), [
-            iaa.Affine(scale=(0.9, 1), mode="symmetric"),
-            iaa.Affine(translate_percent=(0, 0.1), mode="symmetric"),
-            iaa.Affine(rotate=(-15, 15), mode="symmetric"),
-            iaa.Affine(shear=(-5, 5), mode="symmetric")
-        ]),
+        hist_use = config["preprocessing"]["augmentation"]["histogram_method"]
 
-        
-        iaa.SomeOf((0,1),[
-            iaa.GaussianBlur(sigma=(0, 0.5)), # blur images with a sigma of 0 to 0.5
-            iaa.AverageBlur(k=(1, 3)),
-            iaa.MedianBlur(k=1),
+        if hist_use == "rescale":
+            hist_fun = RescaleIntensity
+        elif hist_use == "CLAHE":
+            hist_fun = iaa.CLAHE
+        elif hist_use == "hist_equalization":
+            hist_fun = iaa.AllChannelsHistogramEqualization
+        else:
+            hist_fun = None
+        print("AAAA")
 
-        ]),
-        
-        iaa.SomeOf( (0,2),[
-        iaa.SaltAndPepper((0, 0.015), per_channel=True),
-        iaa.OneOf([iaa.Multiply((0.9, 1.1)),
-        iaa.Multiply((0.9, 1.1), per_channel=0.5)])
+        list_augmenters = [
+            iaa.SomeOf((0,5), [
+                iaa.Affine(scale=(0.9, 1), mode="symmetric"),
+                iaa.Affine(translate_percent=(0, 0.1), mode="symmetric"),
+                iaa.Affine(rotate=(-15, 15), mode="symmetric"),
+                iaa.Affine(shear=(-5, 5), mode="symmetric")
+            ]),
+
+            
+            iaa.SomeOf((0,1),[
+                iaa.GaussianBlur(sigma=(0, 0.5)), # blur images with a sigma of 0 to 0.5
+                iaa.AverageBlur(k=(1, 3)),
+                iaa.MedianBlur(k=1),
+
+            ]),
+            
+            iaa.SomeOf( (0,2),[
+            iaa.SaltAndPepper((0, 0.015), per_channel=True),
+            iaa.OneOf([iaa.Multiply((0.9, 1.1)),
+            iaa.Multiply((0.9, 1.1), per_channel=0.5)])
+            ]
+            )
         ]
-        )
-    ]
 
-    if hist_fun is not None:
-        list_augmenters = list_augmenters + hist_fun(per_channel=False)
-    
-    seq = iaa.SomeOf((0, 5), list_augmenters)
-
-    return seq
-
-class EqualizeHist(Augmenter):
-    def __init__(self,name=None, deterministic=False, random_state=None, per_channel = False):
-        super(EqualizeHist, self).__init__(name=name, deterministic=deterministic, random_state=random_state)
+        if hist_fun is not None:
+            list_augmenters = list_augmenters + hist_fun(per_channel=False)
         
-        self.per_channel = per_channel
+        seq = iaa.SomeOf((0, 5), list_augmenters)
 
-    def _augment_images(self, images, random_state, parents, hooks):
-        for i in range(len(images)):
-            if self.per_channel:
-                tmp_image = images[i]
-                
-                for ch in arange(tmp_image.shape[2]):
-                    tmp_image[: , :, ch] = exposure.equalize_hist(tmp_image[: , :, ch])
+        return seq
+
+    class EqualizeHist(Augmenter):
+        def __init__(self,name=None, deterministic=False, random_state=None, per_channel = False):
+            super(EqualizeHist, self).__init__(name=name, deterministic=deterministic, random_state=random_state)
+            
+            self.per_channel = per_channel
+
+        def _augment_images(self, images, random_state, parents, hooks):
+            for i in range(len(images)):
+                if self.per_channel:
+                    tmp_image = images[i]
                     
-                images[i] = tmp_image
-            else:
-                images[i] = exposure.equalize_hist(images[i])
-        return images
+                    for ch in arange(tmp_image.shape[2]):
+                        tmp_image[: , :, ch] = exposure.equalize_hist(tmp_image[: , :, ch])
+                        
+                    images[i] = tmp_image
+                else:
+                    images[i] = exposure.equalize_hist(images[i])
+            return images
 
-    def _augment_keypoints(self, keypoints_on_images, random_state, parents, hooks):
-        return keypoints_on_images
+        def _augment_keypoints(self, keypoints_on_images, random_state, parents, hooks):
+            return keypoints_on_images
 
-    def get_parameters(self):
-        return [self.p]
+        def get_parameters(self):
+            return [self.p]
 
-class RescaleIntensity(Augmenter):
-    def __init__(self,name=None, deterministic=False, random_state=None, per_channel = False, percentiles = (2, 98)):
-        super(RescaleIntensity, self).__init__(name=name, deterministic=deterministic, random_state=random_state)
-        
-        self.per_channel = per_channel
-        self.percentiles = percentiles
+    class RescaleIntensity(Augmenter):
+        def __init__(self,name=None, deterministic=False, random_state=None, per_channel = False, percentiles = (2, 98)):
+            super(RescaleIntensity, self).__init__(name=name, deterministic=deterministic, random_state=random_state)
+            
+            self.per_channel = per_channel
+            self.percentiles = percentiles
 
-    def _augment_images(self, images, random_state, parents, hooks):
+        def _augment_images(self, images, random_state, parents, hooks):
 
-        for i in range(len(images)):
-            if self.per_channel:
-                tmp_image = images[i]
-                
-                for ch in arange(tmp_image.shape[2]):
-                    p1, p2 = np.percentile(img[: , : , ch], self.percentiles)
-                    tmp_image[: ,: , ch] = exposure.rescale_intensity(tmp_image[: ,: , ch], in_range=(p1, p2))
-
+            for i in range(len(images)):
+                if self.per_channel:
+                    tmp_image = images[i]
                     
-                images[i] = tmp_image
-            else:
-                p1, p2 = np.percentile(images[i], self.percentiles)
-                images[i] = exposure.rescale_intensity(images[i], in_range=(p1, p2))
-        return images
+                    for ch in arange(tmp_image.shape[2]):
+                        p1, p2 = np.percentile(img[: , : , ch], self.percentiles)
+                        tmp_image[: ,: , ch] = exposure.rescale_intensity(tmp_image[: ,: , ch], in_range=(p1, p2))
 
-    def _augment_keypoints(self, keypoints_on_images, random_state, parents, hooks):
-        return keypoints_on_images
+                        
+                    images[i] = tmp_image
+                else:
+                    p1, p2 = np.percentile(images[i], self.percentiles)
+                    images[i] = exposure.rescale_intensity(images[i], in_range=(p1, p2))
+            return images
 
-    def get_parameters(self):
-        return [self.p]
+        def _augment_keypoints(self, keypoints_on_images, random_state, parents, hooks):
+            return keypoints_on_images
+
+        def get_parameters(self):
+            return [self.p]
 
 
 if __name__ == "__main__":
